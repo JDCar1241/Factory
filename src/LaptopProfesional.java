5f357b044f1f148f666e71cd99499c771f4a8a36
JDCar1241 <jcardenasv3.a@upao.edu.pe>
2026-06-27 21:55:46 -0500

    feat: agregacion de LaptopProfesional.java

---
+++ src/LaptopProfesional.java
@@ -0,0 +1,14 @@
+
+public class LaptopProfesional extends Laptop {
+
+    public LaptopProfesional(String marca, String modelo, int anioFabricacion,
+                             String procesador, double tamanioPantalla) {
+        super(marca, modelo, anioFabricacion, procesador, tamanioPantalla);
+    }
+
+    @Override
+    public void ejecutarPrueba() {
+        System.out.println("Esta laptop es para uso profesional.");
+    }
+
+    }
